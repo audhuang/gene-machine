6e2b5c660af43000615a4baa0ca63db3c78a477e
audhuang <audreynhuang@gmail.com>
2017-06-24 04:16:36 -0700

    try run

--- boltzmann.py
+++ boltzmann.py
@@ -1,12 +1,8 @@
 from __future__ import division, print_function
 import numpy as np
 import random 
+from sklearn.linear_model import Lasso
 
-
-
-
-# fix matrix dE
-# set to high energy
 
 
 def random_network(n, mu = 0., sigma = 1.): 
@@ -15,6 +11,21 @@ def random_network(n, mu = 0., sigma = 1.):
 	np.fill_diagonal(network, 0)
 	return network
 
+def sparse_random_network(n, perc, mu = 0., sigma = 1.): 
+	num = int(np.ceil(n**2 * perc)) 
+	random = np.random.normal(mu, sigma, [n, n])
+	indices = np.random.choice(range(n**2), num, replace=False)
+	for i in range(num): 
+		index = indices[i]
+		x = int(np.floor(index / n)) 
+		y = index % n
+		random[x, y] = 0
+
+	network = np.tril(random) + np.tril(random, -1).T
+	np.fill_diagonal(network, 0)
+	return network
+
+# check the math here
 def energy(x, J, h):
 	# print(np.sum(np.dot(J, np.outer(x, x))))
 	# print(np.dot(np.dot(x, J), np.transpose(x)))
@@ -65,6 +76,20 @@ def boltz(J, h, c, n, it, repeat):
 	return x_array, e_array
 
 
+def prob_dist(J, h, c, n, x_all, energies, repeat):
+	p = np.exp(-energies / c)
+	# print('p: ', p)
+	Z = np.sum(p)
+	probabilities = p / Z
+	# print('probabilities: ', probabilities)
+
+	indices = np.random.choice(range(2**n), repeat, replace=True, p=probabilities)
+	# print(indices[:10])
+	x_array = np.take(x_all, indices, axis=0)
+	e_array = np.take(energies, indices)
+	return x_array, e_array
+
+
 def x_to_states(x_array, n, repeat, bin_to_int): 
 	x = ((x_array + 1) / 2).astype(int)
 	# print('x: ', x)
@@ -88,7 +113,6 @@ def states_to_dE(states, n):
 	
 	dE = -c * np.log(dE)
 	return dE
-	# what did we do again if there was zero in state count?
 
 def states_to_dE_one(states, x_all, n, threshold, c): 
 	dE = np.zeros([2 ** n])
@@ -103,8 +127,6 @@ def states_to_dE_one(states, x_all, n, threshold, c):
 			dE[i] = 0.00001
 		S[i] = np.transpose(np.outer(x_all[i], x_all[i]) - np.outer(x_all[base], x_all[base]))
 
-
-	# print('dE": ', dE)
 	dE = -c * np.log(dE)
 	return dE, S
 
@@ -124,68 +146,22 @@ def solve(S, dE):
 	inv = np.linalg.pinv(S)
 	return np.dot(np.linalg.pinv(S), dE) 
 
-def solve_l1(S, dE): 
-	return 
+def solve_l1(S, dE, a): 
+	clf = Lasso(alpha=a, fit_intercept=False)
+	clf.fit(S, dE)
+	return clf.coef_
 
 
 def error(true, pred, n): 
 	return np.sum(np.abs(true - pred)) / n**2
 
 
-def run(n, number, low, high, stride, it, repeat, threshold, out): 
-	bin_to_int = {}
-	int_to_bin = {}
-	x_all = np.zeros([2**n, n])
-	bin = np.zeros([n])
-	for i in range(2 ** n): 
-		temp = "{0:b}".format(i)
-		bin = '0' * (n - len(temp)) + temp
-		bin_to_int[bin] = i
-		int_to_bin[i] = bin
-		x_all[i] = list(bin)
-	x_all = x_all * 2 - 1
 
-	J_dict = {}
-	err_dict = {}
-
-
-	for c_ in np.linspace(low, high+1, stride): 
-		c = 10. ** c_
-		print('c: ', c)
-
-		J_dict[c_] = np.zeros([number, n, n])
-		err_dict[c_] = np.zeros([number])
-		for num in range(number): 
-			J = random_network(n)
-			h = np.zeros([n])
-			
-			x_array, e_array = boltz(J, h, c, n, it, repeat)
-			
-			states = x_to_states(x_array, n, repeat, bin_to_int)
-			
-			dE, S = states_to_dE_one(states, x_all, n, threshold, c)
-
-			S_flat = S.reshape([2**n, n**2])
-			dE_flat = dE
-
-			J_hat = solve(S_flat, dE_flat)
-			J_hat = J_hat.reshape([n, n])
-			
-			err = error(J, J_hat, n)
-			print('error: ', err)
-
-			J_dict[c_][num] = J_hat
-			err_dict[c_][num] = err 
-			np.save(out + './J_dict_' + str(number) + '_' + str(low) + '_' + str(high) + '_' + str(stride), J_dict)
-			np.save(out + './err_dict_' + str(number) + '_' + str(low) + '_' + str(high) + '_' + str(stride), err_dict)
-
-		print('\n')
-	print(err_dict)
 
 
 def main(): 
 	it = 10
-	repeat = 100
+	repeat = 1000
 	n = 2
 	c = 1
 	threshold = 1.
@@ -203,25 +179,31 @@ def main():
 	x_all = x_all * 2 - 1
 
 
+	# J = sparse_random_network(n, 0.75)
 	J = random_network(n)
 	h = np.zeros([n])
+
+	# x_array, e_array = boltz(J, h, c, n, it, repeat)
+	# st = x_to_states(x_array, n, repeat, bin_to_int)
+	# print('st: ', st)
+	
+
 	energies = energy_all(x_all, J, h, c, n)
 	print('energies: ', energies)
-	x_array, e_array = boltz(J, h, c, n, it, repeat)
-	# print('x array:', x_array)
-	# print('e array:', e_array)
+	x_array, e_array = prob_dist(J, h, c, n, x_all, energies, repeat)
+
 	states = x_to_states(x_array, n, repeat, bin_to_int)
-	# states = [0., 47., 41., ]
 	print('states: ', states)
+	
 	dE, S = states_to_dE_one(states, x_all, n, threshold, c)
 	print('dE: ', dE)
-	# print(S)
 
 	S_flat = S.reshape([2**n, n**2])
 	dE_flat = dE
 
 
-	J_hat = solve(S_flat, dE_flat)
+	J_hat = solve_l1(S_flat, dE_flat, 0.1)
+	# J_hat = solve(S_flat, dE_flat)
 	J_hat = J_hat.reshape([n, n])
 	print('J": ', J_hat)
 	print('J: ', J)
--- temp_sim.py
+++ temp_sim.py
@@ -1,26 +1,67 @@
 from __future__ import division, print_function
 import numpy as np
 import random 
-from boltzmann import *
+# from boltzmann import *
+from runs import *
+import matplotlib.pyplot as plt
 
 
-def visualize(): 
-	return
+def visualize(out, number, low, high, stride, outfile): 
+	path = out + 'err_dict_' + str(number) + '_' + str(low) + '_' + str(high) + '_' + str(stride)
+	err_dict = np.load(path + '.npy').item()
 
+	path = './results_l1/' + 'err_dict_l1_' + str(number) + '_' + str(low) + '_' + str(high) + '_' + str(stride)
+	err_dict_l1 = np.load(path + '.npy').item()
+
+	
+	plt.figure()
+	for key in err_dict.keys(): 
+		if key < 3:  
+			plt.scatter([key] * number, np.log(err_dict[key]), color='b', s=1)
+	for key in err_dict_l1.keys(): 
+		if key < 3: 
+			plt.scatter([key] * number, np.log(err_dict_l1[key]), color='r', s=1)
+	plt.ylabel('error')
+	plt.xlabel('temperature')
+	plt.show()
 
 
 def main(): 
-	n = 10
-	number = 5
-	low = -1
-	high = 10
-	stride = 100
-	it = 100
-	repeat = 1000
-	threshold = 1.
-	output = './results/'
+	# n = 10
+	# number = 5
+	# low = -1
+	# high = 5
+	# stride = 20
+	# it = 100
+	# repeat = 1000
+	# threshold = 1.
+	# l1 = False
+	# runvar = False
 
-	run(n, number, low, high, stride, it, repeat, threshold, output)
+	# if l1 == False: 
+	# 	output = './results/'
+	# 	outfile = output + 'vis'
+	# 	if runvar==True: 
+	# 		run(n, number, low, high, stride, it, repeat, threshold, output)
+
+	# elif l1 == True: 
+	# 	output = './results_l1/'
+	# 	outfile = output + 'vis'
+	# 	a = 1.
+	# 	if runvar==True: 
+	# 		run_l1(n, number, low, high, stride, it, repeat, threshold, output, a)
+
+	# visualize(output, number, low, high, stride, outfile)
+
+
+	n = 10
+	number = 10**8
+	low = -1
+	high = 4
+	threshold = 10.
+	output = './results/'
+	run_repeat_temp(n, number, low, high, threshold, output)
+
 
 if __name__ == '__main__':
 	main()

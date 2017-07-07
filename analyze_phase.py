601d9cf58af536c712b740d58dd415506cf2040a
audhuang <audreynhuang@gmail.com>
2017-07-07 16:59:20 -0700

    friday

--- analyze_phase.py
+++ analyze_phase.py
@@ -2,6 +2,7 @@ from __future__ import division, print_function
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+from boltzmann import solve
 
 n = 10
 num_sparse = 20
@@ -10,34 +11,79 @@ def phase_graph(inp, out):
 	results = np.load(inp + '.npy')
 	print(np.shape(results))
 
-	q = results[:, 0]
-	i = results[:, 1]
+	graph_dict = {}
+
+	q = results[:, 0].astype(int)
+	i = results[:, 1].astype(int)
+	err_none = results[:, 4]
+	err_l1 = results[:, 5]
+
+	
 	delta = i / n**2
-	nu = q / i 
-	err_none = results[:, 2]
-	err_l1 = results[:, 3]
-	# print(results[:, 3])
-	# print(np.min(err_l1), np.max(err_none))
+	nu = np.divide(q, i) 
+	
+	for j in range(len(delta)): 
+		key = str(q[j]) + '_' + str(i[j])
+		if key not in graph_dict: 
+			graph_dict[key] = [0, 0]
+		if err_l1[j] < 0.1:
+			graph_dict[key][0] += 1
+		graph_dict[key][1] += 1 
+
+	err = np.zeros(len(q))
+	x = np.zeros(len(q))
+	y = np.zeros(len(q))
+	count = 0
+	for key in graph_dict.keys():
+		lel = key.split('_')
+		xlel = int(lel[0])
+		ylel = int(lel[1])
+		x[count] = ylel / n**2
+		y[count] = xlel / ylel
+		err[count] = graph_dict[key][0] / graph_dict[key][1]
+		count += 1
+	print(xlel, ylel)
+
+
+
+
+	
+
 
 	plt.figure()
-	plt.scatter(delta, nu, c=err_none, s=1, cmap="jet", vmin=0, vmax=1)
+	plt.scatter(x, y, c=err, s=10, cmap="jet", vmin=0, vmax=1, lw=0)
 	plt.colorbar()
 	plt.xlabel('delta')
 	plt.ylabel('nu')
 	plt.title('phase least squares')
 	plt.xlim([0, 1])
 	plt.ylim([0, 1])
-	plt.savefig(out + '_none')
+	plt.savefig(out + '_graph')
 
-	plt.figure()
-	plt.scatter(delta, nu, c=err_l1, s=1, cmap="jet", vmin=0, vmax=1)
-	plt.colorbar()
-	plt.xlabel('delta')
-	plt.ylabel('nu')
-	plt.title('phase lasso')
-	plt.xlim([0, 1])
-	plt.ylim([0, 1])
-	plt.savefig(out + '_l1')
+
+
+
+
+
+	# plt.figure()
+	# plt.scatter(delta, nu, c=err_none, s=1, cmap="jet", vmin=0, vmax=1)
+	# plt.colorbar()
+	# plt.xlabel('delta')
+	# plt.ylabel('nu')
+	# plt.title('phase least squares')
+	# plt.xlim([0, 1])
+	# plt.ylim([0, 1])
+	# plt.savefig(out + '_none')
+
+	# plt.figure()
+	# plt.scatter(delta, nu, c=err_l1, s=1, cmap="jet", vmin=0, vmax=1)
+	# plt.colorbar()
+	# plt.xlabel('delta')
+	# plt.ylabel('nu')
+	# plt.title('phase lasso')
+	# plt.xlim([0, 1])
+	# plt.ylim([0, 1])
+	# plt.savefig(out + '_l1')
 
 
 
--- phase.py
+++ phase.py
@@ -11,6 +11,7 @@ sigma = 1.
 num_sparse=20
 a = 0.001
 
+
 def get_full_S(x_all, n): 
    S = np.zeros([2 ** n, n, n])
    base = np.random.randint(0, 2**n)
@@ -25,6 +26,36 @@ def get_bin_S(n):
    x = np.random.binomial(1, 0.5, [2**n, n**2])
    x = x * 2 - 1
    return x
+
+def graph(x, n, param):  
+   obs = param[0]
+   sparse = param[1]
+
+   J = sparse_rand(n, k)
+   k = np.sum(J != 0)
+
+   S_flat = get_random_S(n).reshape([2**n, n**2])
+   indices = np.random.choice(2**n, obs)
+   S = np.take(S_flat, indices, axis=0)
+
+   dE = np.dot(S, J)
+   dE_rank = len(dE)
+
+   J_none = solve(S, dE)
+   J_l1 = solve_l1(S, dE, a)
+   
+   err_none_0 = error_l0(J, J_none, n, 10**-6)
+   err_l1_0 = error_l0(J, J_l1, n, 10**-6)
+   err_none_2 = np.linalg.norm(J - J_none)
+   err_l1_2 = np.linalg.norm(J - J_l1)
+
+   # results[x] = np.asarray([delta, nu, err_none, err_l1])
+
+   if x % 1000 == 0: 
+      print(x, k, sparse, obs, err_none_0, err_l1_0, err_none_2, err_l1_2)
+
+   return (k, obs, err_none_0, err_l1_0, err_none_2, err_l1_2)
+
 
 
 
@@ -74,12 +105,21 @@ def vary_samples(x, n):
 
 
 def run(n, num, out): 
+   params = np.zeros([n, k, repeat])
+
+   for i in range(1, 10): 
+      params[0] = 0.1 * 
+
+
+
+
    pool = mp.Pool()
-   results = [pool.apply_async(vary_samples, args=(x, n)) for x in range(num)]
+   # results = [pool.apply_async(vary_samples, args=(x, n)) for x in range(num)]
+   results = [pool.apply_async(graph, args=(x, n)) for x in range(num)]
    output = [p.get() for p in results]
    print(np.shape(output))
 
-   np.save(out + 'phase_l0_5_2', output)
+   np.save(out + 'phase_all', output)
 
 
 
